1a06bb527e0c0bb43c6e9116a6143086d0b7bed7
nhatnv@techasians.com <Nhat1998@>
2024-12-24 23:39:40 +0700

    commit

--- script.js
+++ script.js
@@ -1,51 +1,56 @@
-  "object" != typeof window.CP && (window.CP = {}),
-  (window.CP.PenTimer = {
-    programNoLongerBeingMonitored: !1,
-    timeOfFirstCallToShouldStopLoop: 0,
-    _loopExits: {},
-    _loopTimers: {},
-    START_MONITORING_AFTER: 2e3,
-    STOP_ALL_MONITORING_TIMEOUT: 5e3,
-    MAX_TIME_IN_LOOP_WO_EXIT: 2200,
-    exitedLoop: function (E) {
-      this._loopExits[E] = !0;
-    },
-    shouldStopLoop: function (E) {
-      if (this.programKilledSoStopMonitoring) return !0;
-      if (this.programNoLongerBeingMonitored) return !1;
-      if (this._loopExits[E]) return !1;
-      var _ = this._getTime();
-      if (0 === this.timeOfFirstCallToShouldStopLoop)
-        return (this.timeOfFirstCallToShouldStopLoop = _), !1;
-      var o = _ - this.timeOfFirstCallToShouldStopLoop;
-      if (o < this.START_MONITORING_AFTER) return !1;
-      if (o > this.STOP_ALL_MONITORING_TIMEOUT)
-        return (this.programNoLongerBeingMonitored = !0), !1;
-      try {
-        this._checkOnInfiniteLoop(E, _);
-      } catch {
-        return (
-          this._sendErrorMessageToEditor(),
-          (this.programKilledSoStopMonitoring = !0),
-          !0
-        );
-      }
-      return !1;
-    },
+const canvas = document.getElementById("snow");
+const ctx = canvas.getContext("2d");
 
-    _checkOnInfiniteLoop: function (E, _) {
-      if (!this._loopTimers[E]) return (this._loopTimers[E] = _), !1;
-      if (_ - this._loopTimers[E] > this.MAX_TIME_IN_LOOP_WO_EXIT)
-        throw "Infinite Loop found on loop: " + E;
-    },
-    _getTime: function () {
-      return Date.now();
-    },
-  }),
-  (window.CP.shouldStopExecution = function (E) {
-    var _ = window.CP.PenTimer.shouldStopLoop(E);
-    return 
-  }),
-  (window.CP.exitedLoop = function (E) {
-    window.CP.PenTimer.exitedLoop(E);
-  });
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const snowflakes = [];
+for (let i = 0; i < 200; i++) {
+    snowflakes.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        radius: Math.random() * 3 + 1,
+        speed: Math.random() * 2 + 0.5,
+    });
+}
+
+function drawSnowflakes() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    snowflakes.forEach((flake) => {
+        const gradient = ctx.createRadialGradient(
+            flake.x,
+            flake.y,
+            0,
+            flake.x,
+            flake.y,
+            flake.radius
+        );
+        gradient.addColorStop(0, "rgba(255, 255, 255, 1)");
+        gradient.addColorStop(0.5, "rgba(255, 255, 255, 0.8)");
+        gradient.addColorStop(1, "rgba(255, 255, 255, 0)");
+
+        ctx.fillStyle = gradient;
+        ctx.beginPath();
+        ctx.arc(flake.x, flake.y, flake.radius, 0, Math.PI * 2);
+        ctx.fill();
+    });
+}
+
+function updateSnowflakes() {
+    snowflakes.forEach((flake) => {
+        flake.y += flake.speed;
+        if (flake.y > canvas.height) {
+            flake.y = 0;
+            flake.x = Math.random() * canvas.width;
+        }
+    });
+}
+
+function animate() {
+    drawSnowflakes();
+    updateSnowflakes();
+    requestAnimationFrame(animate);
+}
+
+animate();
